885a90290e75346f8cb2255123fbe67c7405c562
SeiyaTakahashi <takahashi.seiya.jp@gmail.com>
2021-10-06 21:11:34 +0900

    ホームを追加

--- app/controllers/api/v1/home_controller.rb
+++
@@ -1,5 +0,0 @@
-class Api::V1::HomeController < ApplicationController
-  def index
-    render json: {}
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  root "api/v1/home#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  namespace 'api', { format: 'json' } do
-    namespace 'v1' do
-      resources :home, only: [:index]
-    end
-  end
+  root "home#index"
+  
+  get 'home/index'
 end
